c35d29710e8ad73adc3d8876c03caebd1afd05c1
194011675+poulsonsam@users.noreply.github.com <c27joshua.poulson@afacademy.af.edu>
2025-04-07 22:38:36 -0600

    Pushing

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -95,20 +95,46 @@ begin
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	
 	-- Next State Logic
-  
+     f_Q_next <= s_floor2 when (f_Q = s_floor1 and i_up_down = '1') else
+                 s_floor3 when (f_Q = s_floor2 and i_up_down = '1') else
+                 s_floor4 when (f_Q = s_floor3 and i_up_down = '1') else
+                 s_floor4 when (f_Q = s_floor4 and i_up_down = '1') else       --going up
+                 
+                 s_floor3 when (f_Q = s_floor4 and i_up_down = '0') else
+                 s_floor2 when (f_Q = s_floor3 and i_up_down = '0') else
+                 s_floor1 when (f_Q = s_floor2 and i_up_down = '0') else
+                 s_floor1 when (f_Q = s_floor1 and i_up_down = '0') else    --going down
+                 
+                 s_floor2;           -- default
+     
+           
 	-- Output logic
-
+    with f_Q select
+    o_floor <= "0001" when s_floor1,
+                "0010" when s_floor2,
+                "0011" when s_floor3,
+                "0100" when s_floor4,
+   
+            
+            "0001" when others; -- default is floor1
 	-------------------------------------------------------------------------------------------------------
 	
 	-- PROCESSES ------------------------------------------------------------------------------------------	
 	
 	-- State register ------------
+	stateregister_process: process(i_clk)
+    begin
+	   if (rising_edge (i_clk) and i_stop = '1') then
+	       if i_reset = '1' then
+                f_Q <= s_floor2;
+            else
+                f_Q <= f_Q_next;
+            end if;
+            end if;
+  end process stateregister_process;	
 	
 	
-	-------------------------------------------------------------------------------------------------------
-	
-	
-
+	-----------------------------------------------------------------------------------------------------
 
 
 end Behavioral;
